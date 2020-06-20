969aa707d5d8ccf0b134c535bbaf0e8e523faffc
delProfudo <bruno@hypermedia.tech>
2020-06-20 13:30:00 +1000

    doing a clean install to try and kick off the account problem again

--- bin/stack-clave.js
+++ bin/stack-clave.js
@@ -7,5 +7,10 @@ const { CertificateStack } = require('../lib/cert-stack')
 
 const app = new cdk.App();
 // new StackClaveStack(app, 'StackClaveStack');
-new ItsABucketStack(app, 'BucketStack')
-new CertificateStack(app, 'certificate-stack')
+// new ItsABucketStack(app, 'BucketStack')
+new CertificateStack(app, 'certificate-stack', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+  }
+})
