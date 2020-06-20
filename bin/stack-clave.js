a27183342a759a0880fee92f66d5fbbf8da2af04
delProfudo <bruno@hypermedia.tech>
2020-06-20 16:17:17 +1000

    trying certificates

--- bin/stack-clave.js
+++ bin/stack-clave.js
@@ -2,15 +2,16 @@
 const cdk = require('@aws-cdk/core');
 
 const { Dns_Stack } = require( '../lib/dns_stack' )
-// const { CertificateStack } = require( '../lib/cert-stack' )
+const { CertificateStack } = require( '../lib/cert-stack' )
 
 const app = new cdk.App();
 
-const STACK_DOMAIN = 'clave.groklobster.io'
+const STACK_DOMAIN = 'bave.groklobster.io'
 
-const dns_stack = new Dns_Stack(app, 'certificate-stack', STACK_DOMAIN, {
+const dns_stack = new Dns_Stack( app, 'dns-stack', STACK_DOMAIN, {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION
   }
 })
+const cert_stack = new CertificateStack( app, 'cert-stack', STACK_DOMAIN )
