99606dcdc453ef588fa716aa4b256c429d89ea29
delProfudo <bruno@hypermedia.tech>
2020-06-20 14:55:16 +1000

    trying first domain creation split before making it complex

--- bin/stack-clave.js
+++ bin/stack-clave.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
-
 const cdk = require('@aws-cdk/core');
-const { StackClaveStack } = require('../lib/stack-clave-stack');
-const { ItsABucketStack } = require('../lib/its-a-bucket')
-const { CertificateStack } = require('../lib/cert-stack')
+
+const { Dns_Stack } = require( '../lib/dns_stack' )
+// const { CertificateStack } = require( '../lib/cert-stack' )
 
 const app = new cdk.App();
-// new StackClaveStack(app, 'StackClaveStack');
-// new ItsABucketStack(app, 'BucketStack')
-new CertificateStack(app, 'certificate-stack', {
+
+const STACK_DOMAIN = 'clave.groklobster.io'
+
+const dns_stack = new Dns_Stack(app, 'certificate-stack', STACK_DOMAIN, {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION
