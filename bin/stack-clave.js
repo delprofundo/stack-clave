94a51cf32ee52b51073a8a0b408da20af4a6e626
delProfundo <bruno@hypermedia.tech>
2020-06-21 11:15:10 +1000

    collection stack workin

--- bin/stack-clave.js
+++ bin/stack-clave.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 const cdk = require('@aws-cdk/core');
 
+const { KeyStack } = require( '../lib/kms.stack')
 const { Dns_Stack } = require( '../lib/dns_stack' )
 const { CertificateStack } = require( '../lib/cert-stack' )
 const { BlobStack } = require( '../lib/s3.stack' )
 const { QueueStack } = require( '../lib/queue.stack' )
+const { CollectionStack } = require( '../lib/collection.stack')
 
 
 const app = new cdk.App();
@@ -17,6 +19,12 @@ const dns_stack = new Dns_Stack( app, 'dns-stack', STACK_DOMAIN, {
     region: process.env.CDK_DEFAULT_REGION
   }
 })
-const blob_stack = new BlobStack( app, 'blob-stack', )
-const queue_stack = new QueueStack( app, 'queue-stack' )
+const key_stack = new KeyStack( app, 'key-stack' )
+const blob_stack = new BlobStack( app, 'blob-stack' )
+const queue_stack = new QueueStack( app, 'queue-stack', {
+  kmsKey: key_stack.queueKey
+})
+const collectionStack = new CollectionStack( app, 'collection-stack', {
+  kmsKey: key_stack.collectionKey
+})
 // const cert_stack = new CertificateStack( app, 'cert-stack', STACK_DOMAIN )
